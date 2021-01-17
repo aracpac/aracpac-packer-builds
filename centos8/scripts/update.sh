65885f602c604fe5c272ca3101ef172b0add193f
Mo Ismailzaia <moismailzai@gmail.com>
2021-01-17 13:05:16 -0800

    update for php8

--- centos8/scripts/update.sh
+++ centos8/scripts/update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -eux
 # forked from https://github.com/chef/bento
 
-dnf config-manager --set-enabled PowerTools;
+dnf config-manager --set-enabled powertools;
 dnf -y update;
 reboot;
 sleep 60;
