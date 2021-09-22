363255035eba2a71bd84519a0b05ca5a0ec0b876
Mo Ismailzai <mo@ismailzai.com>
2021-09-21 21:37:29 -0700

    add many new tools and frameworks

--- ubuntu20/scripts/networking.sh
+++ ubuntu20/scripts/networking.sh
@@ -11,6 +11,6 @@ network:
 EOF
 
 # Disable Predictable Network Interface names and use eth0
-sed -i 's/en[[:alnum:]]*/eth0/g' /etc/network/interfaces;
+# sed -i 's/en[[:alnum:]]*/eth0/g' /etc/network/interfaces;
 sed -i 's/GRUB_CMDLINE_LINUX="\(.*\)"/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0 \1"/g' /etc/default/grub;
 update-grub;
